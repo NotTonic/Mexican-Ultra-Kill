7f92d8a8f8c3d1068b77aaa0f5001d40e81a17af
NotTonic <adrianld2008@gmail.com>
2024-11-10 15:47:36 -0600

    init

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -28,7 +28,7 @@ var jumps = 2
 #PAUSE MENU EVENT
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("escape"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().quit()
 
 
 
